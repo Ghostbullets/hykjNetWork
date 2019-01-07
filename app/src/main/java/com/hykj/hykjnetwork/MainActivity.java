f333b7e48a72676129a29d62797b463e1564ae7c
Ghostbullets <1067339066@qq.com>
2019-01-07 14:45:18 +0800

    1.修改

--- app/src/main/java/com/hykj/hykjnetwork/MainActivity.java
+++ app/src/main/java/com/hykj/hykjnetwork/MainActivity.java
@@ -3,11 +3,21 @@ package com.hykj.hykjnetwork;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hykj.network.yibook.req.AbsReq;
+
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AbsReq req = new AbsReq("") {
+            @Override
+            protected Map<String, String> addHeaders() {
+                return null;
+            }
+        };
     }
 }
