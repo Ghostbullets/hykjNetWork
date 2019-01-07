19cc75d073bb44c445842b9912f5423761657445
Ghostbullets <1067339066@qq.com>
2019-01-07 15:05:33 +0800

    1.代码混淆配置

--- app/src/main/java/com/hykj/hykjnetwork/MainActivity.java
+++ app/src/main/java/com/hykj/hykjnetwork/MainActivity.java
@@ -1,11 +1,7 @@
 package com.hykj.hykjnetwork;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.hykj.network.yibook.req.AbsReq;
-
-import java.util.Map;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AbsReq req = new AbsReq("") {
-            @Override
-            protected Map<String, String> addHeaders() {
-                return null;
-            }
-        };
     }
 }
