9104161f8fb3bf3a68d6e5a69251cf6e94c43433
Ghostbullets <1067339066@qq.com>
2019-01-09 14:05:48 +0800

    1.网络请求可以不传Class<T>参数

--- network/src/main/java/com/hykj/network/xxgj/callback/ObtainCallBack.java
+++ network/src/main/java/com/hykj/network/xxgj/callback/ObtainCallBack.java
@@ -4,8 +4,17 @@ package com.hykj.network.xxgj.callback;
 import com.google.gson.Gson;
 import com.hykj.network.xxgj.rec.BaseRec;
 
+import java.lang.reflect.ParameterizedType;
+
 public abstract class ObtainCallBack<T extends BaseRec> implements BaseCallBack<T> {
     private Class<T> t;
+
+    public ObtainCallBack() {
+        ParameterizedType type = (ParameterizedType) getClass().getGenericSuperclass();
+        if (type != null && type.getActualTypeArguments().length > 0) {
+            t = (Class<T>) type.getActualTypeArguments()[0];
+        }
+    }
 
     public ObtainCallBack(Class<T> t) {
         this.t = t;
--- network/src/main/java/com/hykj/network/yibook/callback/ObtainCallBack.java
+++ network/src/main/java/com/hykj/network/yibook/callback/ObtainCallBack.java
@@ -4,8 +4,17 @@ package com.hykj.network.yibook.callback;
 import com.google.gson.Gson;
 import com.hykj.network.yibook.rec.BaseRec;
 
+import java.lang.reflect.ParameterizedType;
+
 public abstract class ObtainCallBack<T extends BaseRec> implements BaseCallBack<T> {
     private Class<T> t;
+
+    public ObtainCallBack() {
+        ParameterizedType type = (ParameterizedType) getClass().getGenericSuperclass();
+        if (type != null && type.getActualTypeArguments().length > 0) {
+            t = (Class<T>) type.getActualTypeArguments()[0];
+        }
+    }
 
     public ObtainCallBack(Class<T> t) {
         this.t = t;
